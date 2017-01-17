abb43c89c005d738dda99b806f94dc939c328c41
ndriscol <nathan.d.driscoll@gmail.com>
2017-01-17 20:27:16 +1100

    DSC Resource
    complete code

--- Tests/Unit/xCOM.test.ps1
+++ Tests/Unit/xCOM.test.ps1
@@ -26,29 +26,84 @@ try{
     InModuleScope $script:DSCResourceName{
         Describe $Global:DSCResourceName{
 
-            Context "A host entry doesn't exist, and should" {
+            Context "$($Global:DSCResourceName)\Get-TargetResource" {
                 $testParams = @{
-                    SID = "S-1-5-11"
+                    SID = "S-1-5-32-562"
+                    ComObject = 'MachineLaunchRestriction'
+                }
+
+                $testParams2 = @{
+                    SID = "S-1-5-21-21"
+                    ComObject = 'DefaultLaunchPermission'
+                }
+
+                It "It should return true from ensure." {
+                    (get-TargetResource @testParams).Ensure | Should Be $true
+                }
+
+                It "It should return false from ensure." {
+                    (get-TargetResource @testParams2).Ensure | Should Be $false
+                }
+
+                It "It should return the SID of the user account." {
+                    (get-TargetResource @testParams).SID  | Should Be 'S-1-5-32-562'
+                }
+
+                It "It should return the input Comobject." {
+                    (get-TargetResource @testParams).ComObject  | Should Be 'MachineLaunchRestriction'
+                }
+
+             }
+
+            Context "$($Global:DSCResourceName)\Test-TargetResource" {
+                It "should return true from the test method" {
+                    Test-TargetResource @testParams | Should Be $true
+                }
+            }
+
+            Context "$($Global:DSCResourceName)\Set-TargetResource" {
+                $testParams = @{
+                    SID = "S-1-5-21-1606980848-4368372169-844533115-13282"
+                    ComObject = 'MachineLaunchRestriction'
+                }
+
+                $testParams1 = @{
+                    SID = "S-1-5-21-1606980848-4368372169-844533115-13282"
+                    ComObject = 'MachineAccessRestriction'
+                }
+
+                $testParams2 = @{
+                    SID = "S-1-5-21-1606980848-4368372169-844533115-13282"
+                    ComObject = 'DefaultLaunchPermission'
+                }
+
+                $testParams3 = @{
+                    SID = "S-1-5-21-1606980848-4368372169-844533115-13282"
                     ComObject = 'DefaultAccessPermission'
                 }
-                It "should return absent from the get method" {
-                    (get-TargetResource @testParams).Ensure | Should Be $false
+
+                It "should return true from the Set method for MachineLaunchRestriction" {
+                    Set-TargetResource @testParams | Should Be $true
                 }
 
-                It "should return false from the test method" {
-                    Test-TargetResource @testParams | Should Be $false
+                It "should return true from the Set method for MachineAccessRestriction" {
+                    Set-TargetResource @testParams1 | Should Be $true
                 }
 
+                It "should return true from the Set method for DefaultLaunchPermission" {
+                    Set-TargetResource @testParams2 | Should Be $true
+                }
+
+                It "should return true from the Set method for DefaultAccessPermission" {
+                    Set-TargetResource @testParams2 | Should Be $true
+                }
             }
         }
-     }
-  }catch [system.exeception]{
-
-
-    $_
-
     }
 
+  }finally{
+        Restore-TestEnvironment -TestEnvironment $TestEnvironment
+   }
 
 
            
